d7c7c597bd5a46765b481a58ae2bef199c94f977
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-12-06 18:44:21 +0000

    Dynamic classes

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = vue.createApp({
         return {
             showBooks: true,
             books: [
-                {title:'name of the wind', author: 'Patrick rothfuss', img: 'assets/1.jpg'},
-                {title: 'The way of kings', author: 'Brandon Sanderson', img:'assets/2.jpg'},
-                {title: 'The final empire', author: 'Brandon Sanderson', img:'assets/3.jpg'}
+                {title:'name of the wind', author: 'Patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'The way of kings', author: 'Brandon Sanderson', img:'assets/2.jpg', isFav: false},
+                {title: 'The final empire', author: 'Brandon Sanderson', img:'assets/3.jpg', isFav: true}
             ]
         }
     },
